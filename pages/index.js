c16ad68e7b4a76a3a1b60b2ec04a0ad3f93953cd
Satoshi Matsushima <>
2022-07-31 17:31:07 +0900

    updeted hero.js and props and added imagesOn attribute

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Hero from '@/components/hero'
 
 export default function Home() {
-  return <Hero />
+  // imageOn = 論理属性として使用。imageOnが存在すればtrue、なければfalseとなる
+  return <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
 }
