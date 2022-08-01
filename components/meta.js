3f80e0e5c1d93baaa31eb79d636026efdb59f2d0
Satoshi Matsushima <>
2022-08-01 09:21:41 +0900

    created meta.js and constants.js and added meta

---
+++ components/meta.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+// サイトに関する情報
+import { siteMeta } from 'lib/constants'
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } =
+  siteMeta
+
+const Meta = ({ pageTitle, pageDesc }) => {
+  // ページのタイトル
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
+
+  // ページの説明
+  const desc = pageDesc ?? siteDesc
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="og:title" content={`${pageTitle} | ${siteTitle}`} />
+
+      <meta name="description" content={desc} />
+      <meta property="og:description" content={desc} />
+    </Head>
+  )
+}
+export default Meta
