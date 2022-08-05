eba3b2d00fb627d73db93655716f9d2e2e63b241
Satoshi Matsushima <>
2022-08-05 22:18:48 +0900

    limit 5 blogs created when the project build

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -77,37 +77,42 @@ const Post = ({
 }
 
 export async function getStaticPaths() {
-  const allSlugs = await getAllSlugs()
+  const allSlugs = await getAllSlugs(5)
 
   return {
     paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
-    fallback: false,
+    fallback: 'blocking',
   }
 }
 
 export async function getStaticProps(context) {
   const slug = context.params.slug
   const post = await getPostBySlug(slug)
-  const description = extractText(post.content)
-  const eyecatch = post.eyecatch ?? eyecatchLocal
 
-  const { base64 } = await getPlaiceholder(eyecatch.url)
-  eyecatch.blurDataURL = base64
+  if (!post) {
+    return { notFound: true }
+  } else {
+    const description = extractText(post.content)
+    const eyecatch = post.eyecatch ?? eyecatchLocal
 
-  const allSlugs = await getAllSlugs()
-  const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
+    const { base64 } = await getPlaiceholder(eyecatch.url)
+    eyecatch.blurDataURL = base64
 
-  return {
-    props: {
-      title: post.title,
-      publish: post.publishDate,
-      content: post.content,
-      eyecatch: eyecatch,
-      categories: post.categories,
-      description: description,
-      prevPost: prevPost,
-      nextPost: nextPost,
-    },
+    const allSlugs = await getAllSlugs()
+    const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
+
+    return {
+      props: {
+        title: post.title,
+        publish: post.publishDate,
+        content: post.content,
+        eyecatch: eyecatch,
+        categories: post.categories,
+        description: description,
+        prevPost: prevPost,
+        nextPost: nextPost,
+      },
+    }
   }
 }
 
